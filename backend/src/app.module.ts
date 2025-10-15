60aef103b1a0e5b05ace97a87d4845ef5292974a
V-Perotto <vittorio.perotto@outlook.com>
2025-10-15 20:01:07 -0300

    Removendo comentários

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import {
