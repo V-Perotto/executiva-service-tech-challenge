05020cbb71c0657bb17590995f11a6b2349156e8
V-Perotto <vittorio.perotto@outlook.com>
2025-10-16 21:27:51 -0300

    Removendo arquivos para refatoração de código

--- backend/src/auth/jwt.guard.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-
-const JwtAuthGuardBase = AuthGuard('jwt');
-
-@Injectable()
-export class JwtAuthGuard extends JwtAuthGuardBase {}
